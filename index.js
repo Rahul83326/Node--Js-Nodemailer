5b2d109b02ff6ad04f121c785d4a0c91f5187f00
rahulthangamani <rahulthangamani2002@gmail.com>
2022-08-25 19:59:08 +0530

    Initial Commit

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
+const cors = require("cors");
 const app = express();
 // Parse incoming requests data (https://github.com/expressjs/body-parser)
+app.use(cors({ origin: "*" }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 const route = express.Router();
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 
 app.use('/v1', route);
 
@@ -20,8 +22,8 @@ const transporter = nodemailer.createTransport({
     port: 465,
     host: "smtp.gmail.com",
     auth: {
-        user: '<yourmail>@gmail.com',
-        pass: 'xxxxxxxxx',
+        user: 'rahulthangamani2002@gmail.com',
+        pass: 'rahulT@83326',
     },
     tls: {
         rejectUnauthorized: false
@@ -32,7 +34,7 @@ const transporter = nodemailer.createTransport({
 route.post('/text-mail', (req, res) => {
     const {to, subject, text } = req.body;
     const mailData = {
-        from: '<yourmail>@gmail.com',
+        from: 'rahulthangamani2002@gmail.com',
         to: to,
         subject: subject,
         text: text,
@@ -49,12 +51,12 @@ route.post('/text-mail', (req, res) => {
 
 
 route.post('/attachments-mail', (req, res) => {
-    const {to, subject, text } = req.body;
+    const {email, subject, message } = req.body;
     const mailData = {
         from: '<yourmail>@gmail.com',
-        to: to,
-        subject: subject,
-        text: text,
+        to: email,
+        subject: "honeycomb pvt",
+        text: message,
         html: '<b>Hey there! </b><br> This is our first message sent with Nodemailer<br/>',
         attachments: [
             {   // file on disk as an attachment
